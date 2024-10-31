1e17cabedd3c8f3900ceff9b0a727b54eca6746e
Joaososter <joaomartinatti95@gmail.com>
2024-10-30 21:20:22 -0300

    Atualizando comentarios

--- Interface.java
+++ Interface.java
@@ -10,7 +10,7 @@ public class Interface {
         frame.setSize(400, 200);
         frame.setLayout(new BorderLayout());
 
-        JLabel label1 = new JLabel("label1");
+        JLabel label1 = new JLabel("label1");    //Um JPanel é criado. Um JPanel é um contêiner que pode armazenar outros componentes (como botões e campos de texto).
         JTextField textField = new JTextField(15);
 
         JPanel topPanel = new JPanel();
@@ -19,11 +19,11 @@ public class Interface {
         topPanel.add(textField);
 
 
-        JButton button1 = new JButton("Button 1");
+        JButton button1 = new JButton("Button 1");   //Aqui, são criados três botões (JButton) com os rótulos "Button 1", "Button 2" e "Button 3".
         JButton button2 = new JButton("Button 2");
         JButton button3 = new JButton("Button 3");
 
-        JPanel buttonPanel = new JPanel();
+        JPanel buttonPanel = new JPanel(); //Um outro painel é criado, desta vez para os botões.
         buttonPanel.setLayout(new FlowLayout());
         buttonPanel.add(button1);
         buttonPanel.add(button2);
@@ -33,7 +33,7 @@ public class Interface {
         frame.add(topPanel, BorderLayout.NORTH);
         frame.add(buttonPanel, BorderLayout.CENTER);
 
-        frame.setVisible(true);
+        frame.setVisible(true);  //tornar frame invisivel
 
     }
 
